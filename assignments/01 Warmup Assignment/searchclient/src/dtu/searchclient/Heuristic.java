8d3118874815c15684dd234cb463b05cae428ade
Troels Thomsen <tt.ttsoftware@gmail.com>
2016-02-03 11:11:28 +0100

    refactoring

--- assignments/01 Warmup Assignment/searchclient/src/dtu/searchclient/Heuristic.java
+++ assignments/01 Warmup Assignment/searchclient/src/dtu/searchclient/Heuristic.java
@@ -2,66 +2,67 @@ package dtu.searchclient;
 
 import java.util.Comparator;
 
-public abstract class Heuristic implements Comparator< Node > {
+public abstract class Heuristic implements Comparator<Node> {
 
-	public Node initialState;
-	public Heuristic(Node initialState) {
-		this.initialState = initialState;
-	}
+    public Node initialState;
 
-	public int compare( Node n1, Node n2 ) {
-		return f( n1 ) - f( n2 );
-	}
+    public Heuristic(Node initialState) {
+        this.initialState = initialState;
+    }
 
-	public int h( Node n ) {
-		return 0;
-	}
+    public int compare(Node n1, Node n2) {
+        return f(n1) - f(n2);
+    }
 
-	public abstract int f( Node n );
+    public int h(Node n) {
+        return 0;
+    }
 
-	public static class AStar extends Heuristic {
-		public AStar(Node initialState) {
-			super( initialState );
-		}
+    public abstract int f(Node n);
 
-		public int f( Node n ) {
-			return n.g() + h( n );
-		}
+    public static class AStar extends Heuristic {
+        public AStar(Node initialState) {
+            super(initialState);
+        }
 
-		public String toString() {
-			return "A* evaluation";
-		}
-	}
+        public int f(Node n) {
+            return n.g() + h(n);
+        }
 
-	public static class WeightedAStar extends Heuristic {
-		private int W;
+        public String toString() {
+            return "A* evaluation";
+        }
+    }
 
-		public WeightedAStar(Node initialState) {
-			super( initialState );
-			W = 5; // You're welcome to test this out with different values, but for the reporting part you must at least indicate benchmarks for W = 5
-		}
+    public static class WeightedAStar extends Heuristic {
+        private int W;
 
-		public int f( Node n ) {
-			return n.g() + W * h( n );
-		}
+        public WeightedAStar(Node initialState) {
+            super(initialState);
+            W = 5; // You're welcome to test this out with different values, but for the reporting part you must at least indicate benchmarks for W = 5
+        }
 
-		public String toString() {
-			return String.format( "WA*(%d) evaluation", W );
-		}
-	}
+        public int f(Node n) {
+            return n.g() + W * h(n);
+        }
 
-	public static class Greedy extends Heuristic {
+        public String toString() {
+            return String.format("WA*(%d) evaluation", W);
+        }
+    }
 
-		public Greedy(Node initialState) {
-			super( initialState );
-		}
+    public static class Greedy extends Heuristic {
 
-		public int f( Node n ) {
-			return h( n );
-		}
+        public Greedy(Node initialState) {
+            super(initialState);
+        }
 
-		public String toString() {
-			return "Greedy evaluation";
-		}
-	}
+        public int f(Node n) {
+            return h(n);
+        }
+
+        public String toString() {
+            return "Greedy evaluation";
+        }
+    }
 }
