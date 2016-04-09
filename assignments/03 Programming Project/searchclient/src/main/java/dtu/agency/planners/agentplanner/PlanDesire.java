a8dc289c7f45163f512b64d90bfd96e156c0be4e
Troels Thomsen <tt.ttsoftware@gmail.com>
2016-04-09 21:04:22 +0200

    Few tiny changes.  does not actually change anything.

--- assignments/03 Programming Project/searchclient/src/main/java/dtu/agency/planners/agentplanner/PlanDesire.java
+++ assignments/03 Programming Project/searchclient/src/main/java/dtu/agency/planners/agentplanner/PlanDesire.java
@@ -8,18 +8,16 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class PlanDesire { // everything the agent might want to achieve
-    class AgentPlanComparator implements Comparator<Pair<HLAction, PrimitivePlan>> {
-
-        @Override
-        public int compare(Pair<HLAction, PrimitivePlan> o1, Pair<HLAction, PrimitivePlan> o2) {
-            return o2.getValue().size() - o1.getValue().size();
-        }
-    }
 
     PriorityQueue<Pair<HLAction, PrimitivePlan>> desires;
 
     public PlanDesire() {
-        desires = new PriorityQueue<>(new AgentPlanComparator());
+        desires = new PriorityQueue<>(new Comparator<Pair<HLAction, PrimitivePlan>>() {
+            @Override
+            public int compare(Pair<HLAction, PrimitivePlan> o1, Pair<HLAction, PrimitivePlan> o2) {
+                return o2.getValue().size() - o1.getValue().size();
+            }
+        });
     }
 
     public Pair<HLAction, PrimitivePlan> getBestDesire() {
--- assignments/03 Programming Project/searchclient/src/main/java/dtu/agency/services/PlanningLevelService.java
+++ assignments/03 Programming Project/searchclient/src/main/java/dtu/agency/services/PlanningLevelService.java
@@ -1,24 +1,18 @@
 package dtu.agency.services;
 
-import dtu.agency.board.Box;
 import dtu.agency.board.Level;
-import dtu.agency.board.Position;
 
 import java.io.Serializable;
-import java.util.HashMap;
 
 /**
- * Created by koeus on 4/8/16.
- * I think that the purpose of this PlanningLevelService is to support the planning phase of any agent
+ * The purpose of this PlanningLevelService is to support the planning phase of any agent
  */
 public class PlanningLevelService implements Serializable {
 
     private Level level;
-    private HashMap<Box, Position> boxes;
-    private int agent;
 
-    public PlanningLevelService(int agent) {
-        this.agent = agent;
+    public PlanningLevelService() {
+        updateLevel();
     }
 
     public Level getLevel() {
