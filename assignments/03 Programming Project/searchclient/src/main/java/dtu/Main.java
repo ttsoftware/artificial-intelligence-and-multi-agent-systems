7b83bbe59070ee27672091963a3e34d33c82dd7b
Mads Thoudahl <thoudahl@gmail.com>
2016-05-12 20:07:58 +0200

    Added support for more than 10 boxes

--- assignments/03 Programming Project/searchclient/src/main/java/dtu/Main.java
+++ assignments/03 Programming Project/searchclient/src/main/java/dtu/Main.java
@@ -35,10 +35,10 @@ public class Main {
                 // "levels/SAfriendofDFS.lvl",
                 // "levels/SAfriendofBFS.lvl",
                 // "levels/SAsokobanLevel96.lvl",
-                "levels/SAHateful_Three.lvl",
+                // "levels/SAHateful_Three.lvl",
                 // "levels/SAHateful_Eight.lvl",
                 // TODO: - WORKS SOMETIMES!?! - but not if a box is parked on top of a goal
-                // "levels/SALongJourney.lvl", // TODO: Does not work
+                "levels/SALongJourney.lvl", // TODO: Does not work
                 // "levels/SAD1.lvl",
                 // "levels/SAD2.lvl",
                 // "levels/SAD1_multi.lvl",
